bb921b27d605e94449f3cd11ae0187579cac4c56
Mikhail Akopov <mikhail.akopov@getid.ee>
2021-03-23 17:38:46 +0200

    Update GetID Android SDK to version 2.0.0

--- android/app/src/main/java/com/getidexample/GetID.java
+++ android/app/src/main/java/com/getidexample/GetID.java
@@ -1,37 +1,19 @@
 package com.getidexample;
-import android.content.res.Configuration;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.sdk.getidlib.config.ConfigurationPreset;
-import com.sdk.getidlib.config.FlowScreens;
-import com.sdk.getidlib.config.GetIDFactory;
-import com.sdk.getidlib.config.VerificationTypesEnum;
-import com.sdk.getidlib.model.app.document.CountryDocumentConfig;
-import com.sdk.getidlib.model.app.form.FormConfig;
-import com.sdk.getidlib.model.app.form.FormField;
-import com.sdk.getidlib.model.app.liveness.LivenessConfig;
-import com.sdk.getidlib.model.app.selfie.SelfieConfig;
-import com.sdk.getidlib.model.app.thank_you.ThankYouConfig;
-import com.sdk.getidlib.model.app.video_recording.VideoRecordingConfig;
-import com.sdk.getidlib.config.DesignColorSchema;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
+import com.sdk.getidlib.config.GetIDSDK;
+import com.sdk.getidlib.model.app.auth.Token;
 
 public class GetID extends ReactContextBaseJavaModule {
 
-   ReactApplicationContext appContext;  
+    ReactApplicationContext appContext;
 
-   GetID(ReactApplicationContext context) {
-       super(context);
-       appContext = context;
-   }
+    GetID(ReactApplicationContext context) {
+        super(context);
+        appContext = context;
+    }
 
     @Override
     public String getName() {
@@ -39,31 +21,16 @@ public class GetID extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void start(String token, String url) {
-        List<FlowScreens> flowItems = new ArrayList<>();
-        flowItems.add(FlowScreens.SCREEN_CONSENT);
-        flowItems.add(FlowScreens.SCREEN_DOCUMENT);
-        flowItems.add(FlowScreens.SCREEN_SELFIE);
-        flowItems.add(FlowScreens.SCREEN_LIVENESS);
-        ArrayList<VerificationTypesEnum> verificationTypes = new ArrayList<>();
-        verificationTypes.add(VerificationTypesEnum.FACE_MATCHING);
-        verificationTypes.add(VerificationTypesEnum.DATA_EXTRACTION);
-        DesignColorSchema designColorSchema = new DesignColorSchema(null, null, null,
-                null, "#0E1C2C", null, "#FFFFFF", false);
-        ConfigurationPreset configPreset = new ConfigurationPreset(
-                flowItems,
+    public void start(String url, String token, String flowName) {
+        new GetIDSDK().startVerificationFlow(
+                appContext,
+                url,
+                new Token(token),
+                flowName,
                 null,
                 null,
                 null,
-                null,
-                null,
-                verificationTypes,
-                null,
-                designColorSchema);
-        List<Locale> locale = new ArrayList<>();
-        locale.add(Locale.ENGLISH);
-        new GetIDFactory().setup(appContext, configPreset, token,
-                url, locale, null);
+                null
+        );
     }
 }
-

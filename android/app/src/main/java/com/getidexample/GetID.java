d70f69349d737aaa198c4440c08093b3bc91d526
Mikhail Akopov <mikhail.akopov@getid.ee>
2021-09-03 15:51:31 +0300

    Handle callbacks of GetID Android SDK

--- android/app/src/main/java/com/getidexample/GetID.java
+++ android/app/src/main/java/com/getidexample/GetID.java
@@ -1,10 +1,17 @@
 package com.getidexample;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.sdk.getidlib.app.common.receivers.BroadcastReceiverListener;
 import com.sdk.getidlib.config.GetIDSDK;
 import com.sdk.getidlib.model.app.auth.Token;
+import com.sdk.getidlib.model.entity.events.GetIDApplication;
+import com.sdk.getidlib.model.entity.events.GetIDError;
+
+import org.jetbrains.annotations.NotNull;
 
 public class GetID extends ReactContextBaseJavaModule {
 
@@ -20,6 +27,7 @@ public class GetID extends ReactContextBaseJavaModule {
         return "GetID";
     }
 
+
     @ReactMethod
     public void start(String url, String token, String flowName) {
         new GetIDSDK().startVerificationFlow(
@@ -30,7 +38,32 @@ public class GetID extends ReactContextBaseJavaModule {
                 null,
                 null,
                 null,
-                null
+                new GetIDEventListener()
         );
+    }
+}
+
+class GetIDEventListener implements BroadcastReceiverListener {
+
+    private static final String TAG = "GetIDEventListener";
+
+    @Override
+    public void verificationFlowCancel() {
+        Log.d(TAG, "verificationFlowCancel:");
+    }
+
+    @Override
+    public void verificationFlowComplete(@NotNull GetIDApplication getIDApplication) {
+        Log.d(TAG, String.format("verificationFlowComplete: %s", getIDApplication.getApplicationId()));
+    }
+
+    @Override
+    public void verificationFlowFail(@NotNull GetIDError getIDError) {
+        Log.d(TAG, String.format("verificationFlowFail: %s", getIDError.name()));
+    }
+
+    @Override
+    public void verificationFlowStart() {
+        Log.d(TAG, "verificationFlowStart:");
     }
 }

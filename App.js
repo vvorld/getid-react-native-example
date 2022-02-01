3cd198e8c01da9ea43d197b8ca6b1d2eb032230a
Mikhail Akopov <mikhail.akopov.ios@gmail.com>
2022-02-01 10:34:10 +0200

    Get rid of linter warnings in App.js

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { NativeModules, NativeEventEmitter, Button, View } from 'react-native';
+import {NativeModules, NativeEventEmitter, Button, View} from 'react-native';
 
-const { GetID } = NativeModules;
+const {GetID} = NativeModules;
 const GetIDEmitter = new NativeEventEmitter(GetID);
-GetIDEmitter.addListener('GetIDEvent', (body) => console.log(body))
+GetIDEmitter.addListener('GetIDEvent', (body) => console.log(body));
 
 const VerifyButton = () => {
-
   // Note: Don't use your SDK key in the client-side code in the production environment.
   const apiUrl = 'API_URL';
   const sdkKey = 'SDK_KEY';
-  const flowName = "getid-doc-selfie";
+  const flowName = 'getid-doc-selfie';
 
   const getToken = () => {
     return fetch(apiUrl + '/sdk/v2/token', {
@@ -18,26 +17,31 @@ const VerifyButton = () => {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        'x-sdk-key': sdkKey
-      }
+        'x-sdk-key': sdkKey,
+      },
     });
   };
 
   const onPress = () => {
     getToken()
-    .then((response) => response.json())
-    .then((json) => {
-      GetID.start(apiUrl, json.token, flowName);
-    });
+      .then((response) => response.json())
+      .then((json) => {
+        GetID.start(apiUrl, json.token, flowName);
+      });
   };
 
   return (
-    <View style={[{ marginLeft: 20, marginTop: 100, marginRight: 20, backgroundColor: "white" }]}>
-      <Button
-        title="Verify me!"
-        color="blue"
-        onPress={onPress}
-      />
+    <View
+      style={[
+        // eslint-disable-next-line react-native/no-inline-styles
+        {
+          marginLeft: 20,
+          marginTop: 100,
+          marginRight: 20,
+          backgroundColor: 'white',
+        },
+      ]}>
+      <Button title="Verify me!" color="blue" onPress={onPress} />
     </View>
   );
 };
